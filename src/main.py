cd061bc83fff3006c298bf2a1427c5604ce5f613
ljy2855 <ljy2855@naver.com>
2022-02-14 11:10:20 +0900

    Add realtime price parsing and storing

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
+import time
+import numpy as np
+import pandas as pd
 import pyupbit
 import mplfinance as mpf
 from threading import Thread
+
+
 market_code = "KRW-BTC"
 
 
@@ -8,10 +13,14 @@ class BitCoinAPI():
     def __init__(self):
         self.real_time_thread = RealtimePrice()
         self.real_time_thread.daemon = True
+        self.setPrevData()
 
     def initChart(self):
-        self.setPrevData()
-        mpf.plot(self.prev_data, type='candle', mav=(3, 6, 9), volume=True)
+
+        mpf.plot(self.prev_data,  type='candle',
+                 mav=(3, 6, 9), volume=True)
+
+        mpf.show()
 
     def setPrevData(self):
         self.prev_data = pyupbit.get_ohlcv(
@@ -32,19 +41,39 @@ class RealtimePrice(Thread):
 
     def run(self):
         websocket_manager = pyupbit.WebSocketManager("ticker", [market_code])
+        self.data_dict = dict()  # {'timestamp' : array([price...])}
+        self.start_time = None
+
         while True:
             data = websocket_manager.get()
-            print(data['trade_price'])
-            self.ticker = data
+            self.manageDataFrame(data)
+
+    def manageDataFrame(self, data):
+
+        trading_price = data['tp']
+        trading_volume = data['tv']
+        timestamp = data['tms'] / 1000
+        if self.start_time == None:
+            self.start_time = timestamp
+            self.minute_ticker = []
+            self.volume = 0
+        else:
+            if self.start_time + 60 <= timestamp:  # 60초마다 ndarray 형태로 가격 저장
+                self.data_dict[timestamp] = np.array(self.minute_ticker)
+                self.start_time = timestamp
+                print(self.data_dict)
+                self.minute_ticker = []
+                self.volume = 0
+            self.minute_ticker.append(int(trading_price))
+            self.volume += trading_volume
 
 
 if __name__ == '__main__':
 
-    try:
-        bit = BitCoinAPI()
-        # bit.startParsingRealTimePrice()
-        bit.initChart()
-        print(bit.prev_data)
-        input()
-    except KeyboardInterrupt:
-        pass
+    bit = BitCoinAPI()
+    bit.startParsingRealTimePrice()
+    time.sleep(3)
+    # bit.initChart()
+
+    # print(bit.prev_data)
+    time.sleep(1000)
--- upbitWebsocket.md
+++ upbitWebsocket.md
@@ -3,11 +3,33 @@
 * 체결
 * 호가
 
-데이터 전송 방식은 snapshot, realtime으로 나뉘는데
-
- snapshot : 요청했을 때의 데이터
-
- realtime : 실시간 데이터 
+데이터 전송 방식은 snapshot, realtime으로 나뉘는데   
+snapshot : 요청했을 때의 데이터  
+realtime : 실시간 데이터 
  
 
 # data format
+* ty : type
+* cd : coin market
+* op : open price
+* hp : high price
+* lp : low price
+* tp : trade price
+* pcp : prev closing price
+* atp : accumulate trade price
+* c : change
+* cp : change price
+* scp
+* cr
+* scr
+* ab
+* tv
+* atv
+* tdt
+* ttm
+* ttms
+* aav
+* abv
+* h52wp
+* h52wdt
+* tms : timestamp
