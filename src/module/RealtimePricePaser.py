a4a45c7d154f45c176e2d0ede42e148b26fd5f55
ljy2855 <ljy2855@naver.com>
2022-04-09 21:04:22 +0900

    chore: add requirements

--- src/module/RealtimePricePaser.py
+++ src/module/RealtimePricePaser.py
@@ -10,23 +10,24 @@ class RealtimePrice(Thread):
 
     def run(self):
         websocket_manager = pyupbit.WebSocketManager("ticker", [market_code])
-        self.data_dict = dict()  # {'timestamp' : array([price...])}
+        self.data_list = list()
         self.start_time = None
 
         while True:
             data = websocket_manager.get()
+            #print(data)
             self.manageDataFrame(data)
 
     def manageDataFrame(self, data):
 
         # pyupbit lib edited for optimization
-        #trading_price = data['trading_price']
-        #trading_volume = data['trading_volume']
-        #timestamp = data['timestamp'] / 1000
+        trading_price = data['trade_price']
+        trading_volume = data['trade_volume']
+        timestamp = data['timestamp'] / 1000
 
-        trading_price = data['tp']
-        trading_volume = data['tv']
-        timestamp = data['tms'] / 1000
+        # trading_price = data['tp']
+        # trading_volume = data['tv']
+        # timestamp = data['tms'] / 1000
 
         if self.start_time == None:  # init
             self.start_time = timestamp
@@ -34,10 +35,10 @@ class RealtimePrice(Thread):
             self.volume = 0
         else:
             if self.start_time + parse_priod <= timestamp:  # 60초마다 price,volume 저장
-                self.data_dict.update({timestamp: {'prices': np.array(self.minute_ticker),
-                                                   'volume': self.volume}})
+                self.data_list.append({'timestamp' : timestamp, 'prices': np.array(self.minute_ticker),
+                                                   'volume': self.volume})
                 self.start_time = timestamp
-                print(self.data_dict)
+                print(self.data_list)
                 self.minute_ticker = []
                 self.volume = 0
             self.minute_ticker.append(int(trading_price))
